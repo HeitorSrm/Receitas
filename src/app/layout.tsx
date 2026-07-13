78eeb49fc8106af8a726ab9789a304db8e38bd67
Heitor Sebastião Ribeiro de Moura <heitorsebastiaoribeiromoura@gmail.com>
2026-07-12 22:12:02 -0300

    feat: estrutura do footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import Footer from "@/components/Header/Footer";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -24,6 +25,7 @@ export default function RootLayout({
         className={`${inter.variable} antiaçiased`}>
         <Header /> 
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/Header/Footer/index.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer>
+        <div>
+            <p className="text-sm">&copy; 2026 Receitas Deliciosas. Todos os direitos reservados.</p>
+        </div>
+        </footer>
+);
+}
