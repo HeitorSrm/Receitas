c443e4d336597ff561798eb34e84206bec97c830
Heitor Sebastião Ribeiro de Moura <heitorsebastiaoribeiromoura@gmail.com>
2026-07-13 14:18:45 -0300

    feat: aplica responsividade na listagem de receitas, card e footer

--- src/app/receitas/page.tsx
+++ src/app/receitas/page.tsx
@@ -4,10 +4,10 @@ import {recipes} from "@/app/lib/data";
 export default function ReceitasPage() {  
     return (
         <main className="flex grow py-8">
-            <div className="container mx-auto">
-                <h1 className="text-3xl font-bold">Todas as receitas</h1>
+            <div className="container mx-auto px-4">
+                <h1 className="text-3xl font-bold text-center sm:text-left">Todas as receitas</h1>
 
-                <div className="grid grid-cols-3 gap-8 mt-8"> 
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-8"> 
                     {recipes.map((recipe) => (
                     <RecipeCard key={recipe.id} recipe={recipe} />
                 ))}
