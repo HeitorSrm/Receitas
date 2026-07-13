1700b6ff66351e7d4ee28f4bdd28339aa092d2fa
Heitor Sebastião Ribeiro de Moura <heitorsebastiaoribeiromoura@gmail.com>
2026-07-12 22:59:03 -0300

    feat: estrutura da seção Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex grow"> 
-      <h1>Tela Inicial</h1>
+      <div className="container mx-auto">
+        {/* Seção Hero */}
+        <section>
+          <h1>Receitas Deliciosas</h1>
+          <p>Descubra receitas simples e deliciosas para todos os gostos.</p>
+
+          <Link href="/receitas">
+            Ver todas as receitas
+          </Link>
+        </section>
+      </div>
     </main>
   );
 }
