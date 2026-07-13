7fb622ba0dfd4988a4a7dcbdd6d159235bd494ac
Heitor Sebastião Ribeiro de Moura <heitorsebastiaoribeiromoura@gmail.com>
2026-07-12 23:14:36 -0300

    feat: adiciona estilos da secao Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,23 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex grow"> 
-      <div className="container mx-auto">
-        {/* Seção Hero */}
-        <section>
-          <h1>Receitas Deliciosas</h1>
-          <p>Descubra receitas simples e deliciosas para todos os gostos.</p>
+    <main className="flex grow">
+      {/* Seção Hero */}
+      <section className=" bg-orange-50 py-12 w-full text-black">
+        <div className="flex flex-col gap-6 items-center container mx-auto">
+          <h1 className="text-5xl font-bold">Receitas Deliciosas</h1>
+          <p className="text-xl">
+            Descubra receitas simples e deliciosas para todos os gostos.
+          </p>
 
-          <Link href="/receitas">
+          <Link
+            className="bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-3 rounded-lg"
+            href="/receitas"
+          >
             Ver todas as receitas
           </Link>
-        </section>
-      </div>
+        </div>
+      </section>
     </main>
   );
 }
